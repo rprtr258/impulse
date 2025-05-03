0b1a16219825a1a5439ab433297b2a6276ac10fc
Poletansky Viktor <poletansky@gmail.com>
2025-05-03 08:56:00 +0300

    master: open request in command bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -324,6 +324,7 @@ const requestKind = computed(() => {
 
 const commandBarNewRequestKindVisible = ref(false);
 const commandBarVisible = ref(false);
+const commandBarOpenVisible = ref(false);
 const items = [
   {
     label: "Requests",
@@ -339,7 +340,7 @@ const items = [
       },
       {
         label: "Rename current", // TODO: hide if no request selected
-        shortcut: ["Ctrl", "R"],
+        // shortcut: ["Ctrl", "R"],
         perform: () => {
           commandBarVisible.value = false;
 
@@ -350,7 +351,12 @@ const items = [
       },
       {
         label: "Open",
-        shortcut: ["Ctrl", "T"],
+        shortcut: ["Alt", "T"],
+        perform: () => {
+          commandBarVisible.value = false;
+
+          commandBarOpenVisible.value = true;
+        },
       },
       {
         label: "Run",
@@ -398,6 +404,12 @@ const items = [
     ],
   },
 ];
+const openItems = computed(() => {
+  return Object.entries(store.requests).map(([id, preview]) => ({
+    id: id,
+    kind: preview.Kind,
+  }));
+});
 const keys = useMagicKeys();
 const anyModalIsOpen = computed(() => newRequestName.value !== null || renameID.value !== null);
 watch(keys['Alt+K'], (v) => {
@@ -405,6 +417,12 @@ watch(keys['Alt+K'], (v) => {
     return;
   }
   commandBarVisible.value = !commandBarVisible.value;
+});
+watch(keys['Alt+T'], (v) => {
+  if (!v || anyModalIsOpen.value) {
+    return;
+  }
+  commandBarOpenVisible.value = !commandBarOpenVisible.value;
 });
 watch(keys['Escape'], (v) => {
   if (!v) {
@@ -414,6 +432,7 @@ watch(keys['Escape'], (v) => {
   createCancel();
   commandBarVisible.value = false;
   commandBarNewRequestKindVisible.value = false;
+  commandBarOpenVisible.value = false;
 });
 
 
@@ -577,6 +596,76 @@ watch(keys['Escape'], (v) => {
     </ul>
   </template>
 </Command.Dialog>
+<Command.Dialog :visible="commandBarOpenVisible" theme="algolia">
+  <template #header>
+    <Command.Input placeholder="Type or search request id to open" />
+  </template>
+  <template #body>
+    <Command.List>
+      <Command.Empty>No requests found.</Command.Empty>
+      <Command.Item
+        v-for="item in openItems"
+        :data-value="item.id"
+        v-on:select="() => {commandBarOpenVisible = false; selectRequest(item.id);}"
+      >
+        <div>[{{item.kind}}] {{item.id}}</div>
+      </Command.Item>
+    </Command.List>
+  </template>
+  <template #footer>
+    <ul class="command-palette-commands">
+      <li>
+        <kbd class="command-palette-commands-key"><svg width="15" height="15" aria-label="Enter key" role="img">
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.2"
+          >
+            <path
+              d="M12 3.53088v3c0 1-1 2-2 2H4M7 11.53088l-3-3 3-3"
+            />
+          </g></svg></kbd><span class="command-palette-Label">to select</span>
+      </li>
+      <li>
+        <kbd class="command-palette-commands-key"><svg width="15" height="15" aria-label="Arrow down" role="img">
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.2"
+          >
+            <path d="M7.5 3.5v8M10.5 8.5l-3 3-3-3" />
+          </g></svg></kbd><kbd class="command-palette-commands-key"><svg width="15" height="15" aria-label="Arrow up" role="img">
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.2"
+          >
+            <path d="M7.5 11.5v-8M10.5 6.5l-3-3-3 3" />
+          </g></svg></kbd><span class="command-palette-Label">to navigate</span>
+      </li>
+      <li>
+        <kbd class="command-palette-commands-key"><svg width="15" height="15" aria-label="Escape key" role="img">
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.2"
+          >
+            <path
+              d="M13.6167 8.936c-.1065.3583-.6883.962-1.4875.962-.7993 0-1.653-.9165-1.653-2.1258v-.5678c0-1.2548.7896-2.1016 1.653-2.1016.8634 0 1.3601.4778 1.4875 1.0724M9 6c-.1352-.4735-.7506-.9219-1.46-.8972-.7092.0246-1.344.57-1.344 1.2166s.4198.8812 1.3445.9805C8.465 7.3992 8.968 7.9337 9 8.5c.032.5663-.454 1.398-1.4595 1.398C6.6593 9.898 6 9 5.963 8.4851m-1.4748.5368c-.2635.5941-.8099.876-1.5443.876s-1.7073-.6248-1.7073-2.204v-.4603c0-1.0416.721-2.131 1.7073-2.131.9864 0 1.6425 1.031 1.5443 2.2492h-2.956"
+            />
+          </g></svg></kbd><span class="command-palette-Label">to close</span>
+      </li>
+    </ul>
+  </template>
+</Command.Dialog>
 <div
   class="h100"
   :style='{
@@ -649,7 +738,7 @@ watch(keys['Escape'], (v) => {
             :show-line="true"
             :data="treeData"
             :draggable="true"
-            :default-expanded-keys="expandedKeys as unknown as string[]"
+            :default-expanded-keys="expandedKeys"
             v-on:update:expanded-keys="(keys: string[]) => expandedKeys = keys"
             v-on:drop="drag"
             :node-props="treeNodeProps"
